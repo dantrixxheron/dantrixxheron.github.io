dc6ffbd1a5956b4502f02878b1aa2419504656ba
dantrixxheron <danielacmj.04@gmail.com>
2024-11-14 22:45:56 -0600

    Update script change-color function

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 function toggleTheme() {
     const body = document.body;
     const themeToggleButton = document.getElementById("theme-toggle");
+    const btns = document.getElementsByClassName("btn");
 
     // Cambia la clase en el body para alternar el tema
     body.classList.toggle("dark-theme");
@@ -8,9 +9,16 @@ function toggleTheme() {
     // Cambia el estilo y el texto del botón según el tema
     if (body.classList.contains("dark-theme")) {
         themeToggleButton.textContent = "light_mode";
-      themeToggleButton.classList.replace("btn-light", "btn-dark");
+        themeToggleButton.classList.replace("btn-light", "btn-dark");
+        changeTheme(btns, "btn-outline-dark", "btn-outline-light");
     } else {
         themeToggleButton.textContent = "dark_mode";
         themeToggleButton.classList.replace("btn-dark", "btn-light");
+        changeTheme(btns, "btn-outline-light", "btn-outline-dark");
     }
+}
+function changeTheme(array, elementPast, elementNew) {
+    Array.from(array).forEach(e => {
+        e.classList.replace(elementPast, elementNew);
+    });
 }
